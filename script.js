b16368d2815f8181903e5df2e7f36ca4ed18f4c0
adamkr98 <adamkrynski2@gmail.com>
2023-06-06 16:58:41 +0200

    I added another piece of the exercice 2

--- script.js
+++ script.js
@@ -22,16 +22,112 @@
 //     };
 // };
 
-window.onload = () => {
-    let images = document.querySelectorAll("img");
+// window.onload = () => {
+//     let images = document.querySelectorAll("img");
 
-    for( let image of images ) {
-        if( !image.classList.contains(".important")) {
-            image.style.display = "none";
-            // console.log(image + "this one")
-        }
-    }
+//     for( let image of images ) {
+//         if( !image.classList.contains(".important")) {
+//             image.style.display = "none";
+//             // console.log(image + "this one")
+//         }
+//     }
+// }
+
+// window.onload = () => {
+//     let paragraphes = document.querySelectorAll("p");
+
+//     for( let paragraph of paragraphes ) {
+//         console.log(paragraph);
+//     }
+// }
+let colorsArr = [];
+
+window.onload = () => {
+    let paragraphs = document.querySelectorAll("p");
+
+    let randomiseColor = () => {
+
+        for(let paragraph of paragraphs ) {
+
+            let r = Math.floor(Math.random() *256 );
+            let g = Math.floor(Math.random() *256 );
+            let b = Math.floor(Math.random() *256 );
+
+            let randomColor = `rgb(${r}, ${g}, ${b})`;
+            // console.log( randomColor );
+            
+            // while( colorsArr.length  randomColor.length ) {
+
+                if(!colorsArr.includes( randomColor ) && colorsArr.length != randomColor.length ) {
+                    colorsArr.push( randomColor );
+                    console.log(colorsArr);
+
+                } else {
+                    randomiseColor();
+
+                };
+
+                if( colorsArr.length === paragraphs.length ) {
+                    
+                    for( let i = 0; i <= paragraphs.length; i++ ) {
+                        paragraphs[ i ].style.color = colorsArr[ i + 1];
+                    };
+                };
+            // }
+        };
+    };
+
+    
+    console.log(randomiseColor());
 }
+
+  // if(colorsArr.length === paragraphs.length) {
+                    //     for(let i = 0; i <= paragraphs.length; i++ ) {
+                    //         paragraphs[ i ].style.color = colorsArr[ i ];
+                    //     }
+
+    // colorsArr.forEach(element => {
+    //     if(element
+    // });
+
+    
+
+    // let randomiseAllParagColor = () => {
+        
+    //     if()
+    // }
+
+    // for( let paragraph of paragraphes ) {
+    //     for(let i = 0; i <= paragraphes.length; i++ ) {
+    //         let c = randomiseColor()    
+            
+            
+
+
+
+    //          paragraphes[ i ].style.color = c
+            
+    //     }
+
+    // }
+        // let randomColor = "rgb(" + r + "," + g + "," + b + ");"
+        
+        
+        // paragraph.style.color = randomiseColor();
+        
+        //if the parag[i] === rgb XXX and paraga[i + 1]
+        
+            //change randomly color of paragraphes 
+
+
+
+
+
+
+
+    // };
+
+    
 
 
 
